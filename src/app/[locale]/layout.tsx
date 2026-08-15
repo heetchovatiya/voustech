ad3c7054aa30104a9d0fbafe7b33216cdb39841f
Heet Chovatiya <heetchovatiya@gmail.com>
2026-08-15 23:42:45 +0530

    feat(analytics): update Google Tag Measurement ID to G-JMWZQPY8E7 for voustech stream

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -97,14 +97,14 @@ export default async function LocaleLayout({
     >
       <head>
         {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q3JDVXZF7E" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JMWZQPY8E7" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-Q3JDVXZF7E');
+              gtag('config', 'G-JMWZQPY8E7');
             `,
           }}
         />
